9e740b57e1e86746b5d32513027e76dc57b559b6
omjoshi29 <99554159+omjoshi29@users.noreply.github.com>
2022-07-29 19:00:21 +0530

    sort by ratings

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -11,6 +11,7 @@ courseRouter.get("/", async (req, res) => {
   res.status(201).send("welcome to hypernode!");
 });
 
+// get courses
 courseRouter.get("/javascript", async (req, res) => {
   let jslist = await jscourses.find();
   res.status(201).send(jslist);
@@ -26,6 +27,7 @@ courseRouter.get("/python", async (req, res) => {
   res.status(201).send(pythonlist);
 });
 
+// filter by platform
 courseRouter.get("/javascript/filter", async (req, res) => {
   let data;
   if (req.query.platform) {
@@ -46,6 +48,46 @@ courseRouter.get("/python/filter", async (req, res) => {
   let data;
   if (req.query.platform) {
     data = await pythoncourses.find({ platform: req.query.platform });
+  }
+  res.status(201).send(data);
+});
+
+// sort by ratings
+courseRouter.get("/javascript/sort", async (req, res) => {
+  let data;
+  if (req.query.rating) {
+    if (req.query.rating == "rhtl") {
+      req.query.rating = -1;
+    } else {
+      req.query.rating = 1;
+    }
+    data = await jscourses.find().sort({ rating: req.query.rating });
+  }
+  res.status(201).send(data);
+});
+
+courseRouter.get("/java/sort", async (req, res) => {
+  let data;
+  if (req.query.rating) {
+    if (req.query.rating == "rhtl") {
+      req.query.rating = -1;
+    } else {
+      req.query.rating = 1;
+    }
+    data = await javacourses.find().sort({ rating: req.query.rating });
+  }
+  res.status(201).send(data);
+});
+
+courseRouter.get("/python/sort", async (req, res) => {
+  let data;
+  if (req.query.rating) {
+    if (req.query.rating == "rhtl") {
+      req.query.rating = -1;
+    } else {
+      req.query.rating = 1;
+    }
+    data = await pythoncourses.find().sort({ rating: req.query.rating });
   }
   res.status(201).send(data);
 });
