04fc16c0b1aadf0f9a5084a4f4fcc7c62f9029e9
omjoshi29 <99554159+omjoshi29@users.noreply.github.com>
2022-07-29 23:47:28 +0530

    websocket

--- index.js
+++ index.js
@@ -26,11 +26,14 @@ io.on("connection", async (ws) => {
   ws.on("message", async (msg) => {
     if (!msg) {
       ws.emit("message", { server: "give valid input", client: "" });
-    } else if (msg.includes("ello")) {
+    } else if (msg.includes("ello") || msg.includes("hi")) {
       let data = { client: msg, server: "what is your name?" };
       Chatdata(data);
       io.emit("message", data);
-    } else if (msg.includes("I am") || msg.includes("i am")) {
+    } else if (
+      !msg.includes("fine") &&
+      (msg.includes("I am") || msg.includes("i am"))
+    ) {
       msg = msg.split(" ")[2];
       let data = { client: msg, server: `hello ${msg}, how are you` };
       Chatdata(data);
